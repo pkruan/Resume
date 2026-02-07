efee0318b596ad573cec187c4945351af3224cc9
pkruan <ssnan7780@gmail.com>
2026-02-07 22:16:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-function styleProfessional() {
-  document.getElementById("resumeText").value =
-  "Experienced web developer specializing in scalable and secure web applications.";
+ function generateProfile() {
+
+  let name = document.getElementById("name").value;
+  let job = document.getElementById("job").value;
+  let skills = document.getElementById("skills").value;
+  let exp = document.getElementById("experience").value;
+  let goal = document.getElementById("goal").value;
+
+  if(name == "" || job == "") {
+    alert("กรอกชื่อและอาชีพก่อน");
+    return;
+  }
+
+  let profile = `
+${name} is a motivated ${job}.
+
+Skills:
+${skills}
+
+Experience:
+${exp}
+
+Future Goal:
+${goal}
+
+This profile was generated using AI Resume Generator.
+`;
+
+  document.getElementById("result").innerText = profile;
 }
